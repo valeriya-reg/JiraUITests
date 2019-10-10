470ff018227f8ed471c70feaf14ee0d8b3b625f5
regrut <valeriianebo@gmail.com>
2019-10-10 16:13:03 +0300

    Add CreateIssue Test

--- src/JiraTests.java
+++ src/JiraTests.java
@@ -16,10 +16,21 @@ public class JiraTests {
     this.driver = new ChromeDriver();
   }
 
+  // Login
   By userNameInput = By.id("login-form-username");
   By passwordInput = By.id("login-form-password");
   By loginButton = By.id("login-form-submit");
   By dashboard = By.id("dashboard");
+
+  // Create Issue
+  By createIssueButton = By.id("create_link");
+  By projectInput = By.id("project-field");
+  By issueTypeInput = By.id("issuetype-field");
+  By summaryInput = By.id("summary");
+  By dataModeSource = By.xpath("//li[@data-mode='source']");
+  By descriptionInput = By.xpath("//textarea[@name='description']");
+  By createButton = By.id("create-issue-submit");
+  By issueSuccessfullyCreated = By.xpath("//div[@class='aui-message closeable aui-message-success aui-will-close']");
 
   @Test
   public void loginToJiraTest() {
@@ -30,6 +41,31 @@ public class JiraTests {
     driver.findElement(loginButton).click();
 
     Assert.assertTrue(driver.findElement(dashboard).isDisplayed());
+  }
+
+  @Test
+  public void createIssueTest() throws InterruptedException {
+      this.driver.get("https://jira.hillel.it/login.jsp");
+
+      driver.findElement(userNameInput).sendKeys("Valeria_Regrut");
+      driver.findElement(passwordInput).sendKeys("Valeria_Regrut");
+      driver.findElement(loginButton).click();
+
+      driver.findElement(createIssueButton).click();
+      Thread.sleep(3000);
+
+      driver.findElement(projectInput).sendKeys("QAAUTO-8", Keys.RETURN);
+      Thread.sleep(2000);
+      driver.findElement(issueTypeInput).sendKeys("Task", Keys.RETURN);
+      Thread.sleep(2000);
+      driver.findElement(summaryInput).sendKeys("Auto Test");
+      driver.findElement(dataModeSource).click();
+      driver.findElement(descriptionInput).sendKeys("Auto Test");
+
+      driver.findElement(createButton).click();
+      Thread.sleep(2000);
+
+      Assert.assertTrue(driver.findElement(issueSuccessfullyCreated).isDisplayed());
   }
 
   @AfterTest
